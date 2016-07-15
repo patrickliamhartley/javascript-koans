10d121cee2b499f3c0cf60828d242eb420636059
Patrick Hartley <patrickliamhartley@gmail.com>
2016-07-15 03:48:05 -0400

    Third and fourth fix

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -15,7 +15,7 @@ describe("About Expects", function() {
 
   // Some ways of asserting equality are better than others.
   it("should assert equality a better way", function() { 
-	  var expectedValue = FILL_ME_IN;
+	  var expectedValue = 2;
 	  var actualValue = 1 + 1;
 	
   // toEqual() compares using common sense equality.
@@ -24,7 +24,7 @@ describe("About Expects", function() {
 
   // Sometimes you need to be precise about what you "type".
   it("should assert equality with ===", function() { 
-	  var expectedValue = FILL_ME_IN;
+	  var expectedValue = "2";
 	  var actualValue = (1 + 1).toString();
 	
   // toBe() will always use === to compare.
